dac4db92c52254f2381ba5b43b6adaaec37d6aae
Krishna <krishnaqmar@gmail.com>
2017-09-03 09:20:40 +0530

    happy for now

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import config
+import feedparser
+
+rss_feed_links = config.rss_feed
+results_dict = {}
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def rss_reader():
+    for feed_link in rss_feed_links:
+        d = feedparser.parse(feed_link)
+        print("Total entries: ", len(d.entries))
+        print("--------------------------------")
+        for entry in d.entries:
+            print("--------------------------------")
+            print("Title :", entry.title)
+            print("Link :", entry.link)
+            results_dict[entry.title] = entry.link
+
+    return render_template('index.html', results=results_dict)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ config.py
@@ -0,0 +1,2 @@
+rss_feed = ['https://alvinalexander.com/rss.xml', 'http://feeds.feedburner.com/catonmat'
+                 ,'https://www.toptal.com/developers/blog.rss', 'https://feeds.feedburner.com/codinghorror']
